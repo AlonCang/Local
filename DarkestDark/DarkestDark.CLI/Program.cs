947fa2946a53c9db50843ee4af24a439cd18a3cb
Alon Cang <alontsang@gmail.com>
2019-12-29 17:14:55 +0200

    I did some ASCII art and added a little bit of code, just another switch iteration and placed a couple of goto's.

--- DarkestDark/DarkestDark.CLI/Program.cs
+++ DarkestDark/DarkestDark.CLI/Program.cs
@@ -6,10 +6,38 @@ namespace DarkestDark.CLI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("-=DARKEST DARK=- \n You are a badass inquisitor standing before a dark mansion. \n It is your duty to vanquish whatever evil lurks inside!" +
-                "\n What do you do? \n 1. Open the door carefully \n 2.Kick down the door!" );
-            var choice = Console.ReadLine();
-            switch (choice)
+        start1:;
+            Console.WriteLine("==============================================================");
+            Console.WriteLine("=======================-=DARKEST DARK=-======================= " +
+                "\n==============================================================" +
+                " \n You are a badass inquisitor standing before a dark mansion." +
+                " \n It is your duty to vanquish whatever evil lurks inside!" +
+                "\n What do you do? \n 1. Open the door carefully \n 2. Kick down the door!" );
+            Console.WriteLine("==============================================================");
+            var choice01 = Console.ReadLine();
+            switch (choice01)
+            {
+                case "1":
+                    Console.WriteLine("1");
+                    break;
+                case "2":
+                    Console.WriteLine("2");
+                    break;              
+                case "quit":
+                    Console.WriteLine("Quit.");
+                    return;
+                default:
+                    Console.WriteLine("Invalid input, please choose from available options.");
+                    goto start1;
+            }
+
+            start2:;
+            Console.WriteLine("You are now in a spooky entrance hall. There are 3 doors. What do you do?" +
+                " \n 1. Go to the closet room." +
+                " \n 2. Go to the kitchen. " +
+                " \n 3. Go to the living room.");
+            var choice02 = Console.ReadLine();
+            switch (choice02)
             {
                 case "1":
                     Console.WriteLine("1");
@@ -17,11 +45,15 @@ namespace DarkestDark.CLI
                 case "2":
                     Console.WriteLine("2");
                     break;
+                case "3":
+                    Console.WriteLine("3");
+                    break;
                 case "quit":
                     Console.WriteLine("Quit.");
                     return;
                 default:
-                    break;
+                    Console.WriteLine("Invalid input, please choose from available options.");
+                    goto start2;                   
             }
 
             Console.WriteLine("Run Completed successfully");
