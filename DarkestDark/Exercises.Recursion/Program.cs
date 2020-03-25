2e442bf4e23ee4a7b9a23fc8c36a9300854d6641
Nathan Dortman <relvox@gmail.com>
2020-03-25 22:16:34 +0200

    fast

--- DarkestDark/Exercises.Recursion/Program.cs
+++ DarkestDark/Exercises.Recursion/Program.cs
@@ -6,7 +6,7 @@ namespace Exercises.Recursion
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             var letters = new string[] { "A", "B", "C", "D", "X" };
             Console.WriteLine("Hello World!");
@@ -30,5 +30,75 @@ namespace Exercises.Recursion
             if (n == 1) { return 1; }
             return n * Factorial(n - 1);
         }
+
+        static void Main(string[] args)
+        {
+            var tree = new BinaryTree
+            {
+                Root = new BinaryTreeNode
+                {
+                    Value = 7,
+                    Left = new BinaryTreeNode
+                    {
+                        Value = 5,
+                        Left = new BinaryTreeNode
+                        {
+                            Value = 2,
+                            Right = new BinaryTreeNode { Value = 4 },
+                        },
+                        Right = new BinaryTreeNode { Value = 6 },
+                    },
+                    Right = new BinaryTreeNode
+                    {
+                        Value = 9,
+                        Left = new BinaryTreeNode { Value = 8 },
+                        Right = new BinaryTreeNode { Value = 10 },
+                    },
+                }
+            };
+        }
     }
+
+    class BinaryTreeNode
+    {
+        public int Value;
+        public BinaryTreeNode Left;
+        public BinaryTreeNode Right;
+
+        
+    }
+
+    class BinaryTree
+    {
+        public BinaryTreeNode Root;
+
+        public bool Exists(int num)
+        {
+            return RecuriseExists(Root, num);
+        }
+
+        public bool RecuriseExists(BinaryTreeNode aaaa, int num) {
+
+            if (aaaa.Value == num)
+            {
+                return true;
+            }
+            else if (num < aaaa.Value && aaaa.Left != null)
+            {
+                return RecuriseExists(aaaa.Left, num);
+            }
+            else if (num > aaaa.Value && aaaa.Right != null)
+            {
+                return RecuriseExists(aaaa.Right, num);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+
+
+
 }
