7ef5910af0788cc9f6adbb976e4fb7d5a0b9f9dc
Nathan Dortman <relvox@gmail.com>
2020-04-03 20:24:05 +0300

    STUDY THIS
    ask questions or die

--- DarkestDark/Factorio.Mess/Program.cs
+++ DarkestDark/Factorio.Mess/Program.cs
@@ -11,10 +11,21 @@ namespace Factorio.Mess
     {
         static void Main(string[] args)
         {
-            
-            
-
-            
+            var recipes = Recipe.LoadRecipes("../../../Recipes");
+            foreach (var rec in recipes)
+            {
+                Console.WriteLine(rec.Name);
+                Console.WriteLine("- Inputs:");
+                foreach (var inp in rec.Input)
+                {
+                    Console.WriteLine($"  {inp.Key}: {inp.Value}");
+                }
+                Console.WriteLine("- Outputs:");
+                foreach (var outp in rec.Output)
+                {
+                    Console.WriteLine($"  {outp.Key}: {outp.Value}");
+                }                
+            }
         }
     }
 }
