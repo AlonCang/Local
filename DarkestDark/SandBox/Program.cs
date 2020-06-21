a0d98ac2d7a7a257dfad2701704507bad90dab3c
Nathan Dortman <relvox@gmail.com>
2020-06-21 19:55:43 +0300

    A more Idiomatic style.

--- DarkestDark/SandBox/Program.cs
+++ DarkestDark/SandBox/Program.cs
@@ -11,7 +11,6 @@ namespace SandBox
     {
         public List<string> Names;
         public string Location;
-        public List<string> bloops;
     }
     public class OutputLine
     {
@@ -22,71 +21,63 @@ namespace SandBox
 
     class Program
     {
-        public static List<InputLine> ReadInput(string s)
-        {
-            List<InputLine> result = new List<InputLine>();
-            // Split string into lines and save
-            var lines = s.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-
-            // Massage array of strings into a list of Inputlines 
-            foreach (var line in lines)
-            {
-                var parts = line.Split('@', '&');
-                var location = parts.Last();
-                var names = parts.Take(parts.Length - 1);
-                InputLine item = new InputLine();
-                // Turn the contents of names into a list and save it into Names field of item
-                item.Names = names.ToList();
-                item.Location = location;
-                result.Add(item);
-            }
-
-            // return a list of InputLines
-            return result;
-        }
         static void Main(string[] args)
         {
             string s = @"Moshe @ Israel 
                         Boris! & Marisa @ Russia
                         Halil & Lara & Ford @ Egypt";
-
             var inputLines = ReadInput(s);
-
             var outputlines = Massage(inputLines);
-
-            // Print output
             PrintOutput(outputlines);
         }
-        private static List<OutputLine> Massage(List<InputLine> inputLines)
-        {
-            List<OutputLine> result = new List<OutputLine>();
 
+        /// <summary>
+        /// Massage text <see cref="string"/> into <see cref="InputLine"/>s.
+        /// </summary>
+        /// <param name="inputText">Content of entire file(s) (incl. \n \r).</param>
+        /// <returns>An enumeration over <see cref="InputLine"/>s.</returns>
+        public static IEnumerable<InputLine> ReadInput(string inputText)
+        {
+            foreach (var line in inputText.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var parts = line.Split('@', '&');
+                yield return new InputLine
+                {
+                    Names = parts.Take(parts.Length - 1).ToList(),
+                    Location = parts.Last()
+                };
+            }
+        }
+        /// <summary>
+        /// Massage <see cref="InputLine"/>s into <see cref="OutputLine"/>s.
+        /// </summary>
+        /// <param name="inputLines"></param>
+        /// <returns></returns>
+        private static IEnumerable<OutputLine> Massage(IEnumerable<InputLine> inputLines)
+        {
             foreach (var line in inputLines)
             {
                 foreach (var name in line.Names)
                 {
-                    OutputLine temp = new OutputLine();
-                    temp.Name = name.Trim().Trim('!');
-                    temp.IsDead = name.Contains("!");
-                    temp.Location = line.Location.Trim();
-                    result.Add(temp);
+                    yield return new OutputLine
+                    {
+                        Name = name.Trim('!', ' ', '\t'),
+                        IsDead = name.Contains("!"),
+                        Location = line.Location.Trim()
+                    };
                 }
             }
-            return result;
         }
 
-        private static void PrintOutput(List<OutputLine> outputLines)
+        /// <summary>
+        /// Display all <see cref="OutputLine"/>s.
+        /// </summary>
+        /// <param name="outputLines">An enumeration of <see cref="OutputLine"/>s.</param>
+        private static void PrintOutput(IEnumerable<OutputLine> outputLines)
         {
             foreach (var line in outputLines)
             {
-                if (line.IsDead)
-                {
-                    Console.WriteLine($"{line.Name} is dead in {line.Location}");
-                }
-                else
-                {
-                    Console.WriteLine($"{line.Name} is in {line.Location}");
-                }
+                Console.WriteLine($"{line.Name} is {(line.IsDead ? "dead" : "")} in {line.Location}");
             }
         }
     }
